b80f12886329e21b56e2b8f8aa5f897880a5db88
Meiddiel <tokymickaelrivo@gmail.com>
2024-11-22 00:21:04 +0300

    update experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,21 +1,34 @@
 import React from "react";
+import "../Styles/Experience.css";
 
 const Experience = ({ experience }) => {
-  const { position, company, period } = experience;
+  const { company, location, date, jobTitle, responsibilities } = experience;
 
   return (
-    <section>
+    <section className="experience">
       <h3>Experiences</h3>
-      <lu>
+      <div className="timeline">
         {experience.map((job, index) => (
-          <li key={index}>
-            <h4>
-              {job.position} at {job.company}
-            </h4>
-            <p>{job.period}</p>
-          </li>
+          <div className="timeline-item" key={index}>
+            <div className="timeline-dot"></div>
+            <div className="timeline-content">
+              <div className="experience-details">
+                <p className="company">{job.company}</p>
+                <p className="location">{job.location}</p>
+                <p className="date">{job.date}</p>
+              </div>
+              <div className="role-details">
+                <h4 className="job-title">{job.jobTitle}</h4>
+                <lu>
+                  {job.responsibilities.map((item, idx) => (
+                    <li key={idx}>- {item}</li>
+                  ))}
+                </lu>
+              </div>
+            </div>
+          </div>
         ))}
-      </lu>
+      </div>
     </section>
   );
 };
