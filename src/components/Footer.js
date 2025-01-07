e902350bff4cb4722e7c57d65789d6c5800ded70
Meiddiel <tokymickaelrivo@gmail.com>
2025-01-07 14:39:55 +0300

    Fix download button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,49 @@ const Footer = ({ printRef }) => {
     const input = printRef.current;
     if (input) {
       html2canvas(input, { scale: 2 }).then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jsPDF();
+        const pdf = new jsPDF("p", "mm", "A4");
 
         const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+        const pdfHeight = pdf.internal.pageSize.getHeight();
 
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+        const canvasWidth = canvas.width;
+        const canvasHeight = canvas.height;
+
+        const ratio = canvasWidth / pdfWidth;
+
+        let position = 0;
+        let remainingHeight = canvasHeight;
+
+        while (remainingHeight > 0) {
+          const pageCanvas = document.createElement("canvas");
+          pageCanvas.width = canvasWidth;
+          pageCanvas.height = pdfHeight * ratio;
+
+          const pageContext = pageCanvas.getContext("2d");
+          pageContext.drawImage(
+            canvas,
+            0,
+            position * ratio,
+            canvasWidth,
+            pdfHeight * ratio,
+            0,
+            0,
+            canvasWidth,
+            pdfHeight * ratio
+          );
+
+          const pageData = pageCanvas.toDataURL("image/png");
+          pdf.addImage(pageData, "PNG", 0, 0, pdfWidth, pdfHeight);
+
+          remainingHeight -= pdfHeight * ratio;
+          position += pdfHeight;
+
+          if (remainingHeight > 0) {
+            pdf.addPage();
+          }
+        }
+
+        // (canvas.height * pdfWidth) / canvas.width;
         pdf.save("resume.pdf");
       });
     }
